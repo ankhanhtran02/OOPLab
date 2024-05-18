02dbc126b713706c323881d86cedace2fe2f2604
ankhanhtran02 <ankhanhtran02@gmail.com>
2024-05-18 16:22:40 +0700

    Updated the Store class to work with Media

--- AimsProject/src/main/java/hust/soict/dsai/test/store/StoreTest.java
+++ AimsProject/src/main/java/hust/soict/dsai/test/store/StoreTest.java
@@ -9,11 +9,11 @@ public class StoreTest {
         DigitalVideoDisc d1 = new DigitalVideoDisc("Whisper of the Heart", "Animation", "Yoshifumi Kondo", 111, 8.3f);
         DigitalVideoDisc d2 = new DigitalVideoDisc("Contratiempo", "Crime", "Oriol Paulo", 106, 30.0f);
 //        DigitalVideoDisc d3 = new DigitalVideoDisc("Scorpion", "Crime", 25.59f);
-        store.addDVD(d1);
-        store.addDVD(d2);
+//        store.addDVD(d1);
+//        store.addDVD(d2);
 //        store.addDVD(d3);
         store.print();
-        store.removeDVD(d2);
+//        store.removeDVD(d2);
         store.print();
 
     }
