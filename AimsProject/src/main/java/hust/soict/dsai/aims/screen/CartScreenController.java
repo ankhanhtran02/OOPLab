a05c0125bddc7b7bd99853f7fec0a004a9c1a1f5
ankhanhtran02 <ankhanhtran02@gmail.com>
2024-06-01 22:27:40 +0700

    Update buttons

--- AimsProject/src/main/java/hust/soict/dsai/aims/screen/CartScreenController.java
+++ AimsProject/src/main/java/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -2,7 +2,10 @@ package hust.soict.dsai.aims.screen;
 
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.media.Media;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -17,7 +20,10 @@ public class CartScreenController {
     private TableColumn<Media, String> colMediaCategory;
     @FXML
     private TableColumn<Media, Float> colMediaCost;
-
+    @FXML
+    private Button btnPlay;
+    @FXML
+    private Button btnRemove;
     public CartScreenController(Cart cart) {
         super();
         this.cart = cart;
@@ -29,6 +35,20 @@ public class CartScreenController {
         colMediaCategory.setCellValueFactory(new PropertyValueFactory<Media, String>("category"));
         colMediaCost.setCellValueFactory(new PropertyValueFactory<Media, Float>("cost"));
         tblMedia.setItems(this.cart.getItemOrdered());
+
+        btnPlay.setVisible(false);
+        btnRemove.setVisible(false);
+
+        tblMedia.getSelectionModel().selectedItemProperty().addListener(
+            new ChangeListener<Media>() {
+                @Override
+                public void changed(ObservableValue<? extends Media> observableValue, Media oldValue, Media newValue) {
+                    if (newValue != null) {
+                        updateButtonBar(newValue);
+                    }
+                }
+            }
+        );
     }
 
 }
