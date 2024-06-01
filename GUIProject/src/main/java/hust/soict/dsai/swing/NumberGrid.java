549734245ad50f616299b641dd802fffe978db8e
ankhanhtran02 <ankhanhtran02@gmail.com>
2024-06-01 12:15:06 +0700

    Add buttons

--- GUIProject/src/main/java/hust/soict/dsai/swing/NumberGrid.java
+++ GUIProject/src/main/java/hust/soict/dsai/swing/NumberGrid.java
@@ -35,4 +35,26 @@ public class NumberGrid extends JFrame{
         setSize(200, 200);
         setVisible(true);
     }
+
+    void addButtons(JPanel panelButtons) {
+        ButtonListener btnListener = new ButtonListener();
+        for (int i = 1; i <= 9; i++) {
+            btnNumbers[i] = new JButton("" + i);
+            panelButtons.add(btnNumbers[i]);
+            btnNumbers[i].addActionListener(btnListener);
+        }
+
+        btnDelete = new JButton("DEL");
+        panelButtons.add(btnDelete);
+        btnDelete.addActionListener(btnListener);
+
+        btnNumbers[0] = new JButton("0");
+        panelButtons.add(btnNumbers[0]);
+        btnNumbers[0].addActionListener(btnListener);
+
+        btnReset = new JButton("C");
+        panelButtons.add(btnReset);
+        btnReset.addActionListener(btnListener);
+    }
+
 }
